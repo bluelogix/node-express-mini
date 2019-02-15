b02de91b8ee93d5d97efbbaac15f48fd71686caf
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-02-15 14:17:13 -0500

    node-express-mini

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // implement your API here
+
+const express = require('express');
+
+const server = express();
+
+const db = require('./data/db.js');
+
+//uses the middleware 
+server.use(express.json());
+
+//START OF ROUTES
+
+server.get('/', (req, res) => {
+    res.send('This is the mini-express project');
+});
+
+//listening to the server 
+server.listen(4000, () => {
+    console.log('\n*** Running on port 4000 ***\n');
+});
+
