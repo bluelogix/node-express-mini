0fd3c8f5b799ed035b64a75072e242c1bb2c6667
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-02-15 16:24:03 -0500

    working on post and delete

--- index.js
+++ index.js
@@ -15,6 +15,32 @@ server.get('/', (req, res) => {
     res.send('This is the mini-express project');
 });
 
+//CRUD OPERATIONS 
+
+server.get('/api/users', (req, res) => {
+    db.find().then(users => {
+        res.status(200).json(users);
+    }).catch(err => {
+        res.status(err.code).json({ success: false, message: 'The users information could not be retrieved'})
+    })
+})
+
+server.post('/api/users', (req, res) => {
+   const { name, bio } = req.body;
+    if( !name && !bio ) {
+        res.status(400).json({errorMessage: 'Please provide name and bio for the user.'})
+    } else {
+        db.add({ name, bio})
+        .then(user => {
+        res.status(201).json({success: true, user});
+    })
+    .catch(({ code, message }) => {
+        res.status(code).json({ success: false, message})
+})
+
+  
+    
+
 //listening to the server 
 server.listen(4000, () => {
     console.log('\n*** Running on port 4000 ***\n');
